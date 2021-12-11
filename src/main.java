17381b4093f097354f97bfb63845c043a48db3a2
berkecankocyigit <39878835+berkecankocyigit@users.noreply.github.com>
2021-12-12 02:44:36 +0300

    debuging

--- src/main.java
+++ src/main.java
@@ -12,8 +12,26 @@ public class main {
             .connectTimeout(Duration.ofSeconds(10))
             .build();
     public static void main(String[] Args) throws IOException, InterruptedException {
-        Store[] stores;
-        Store store1=new Store();
+        Store[] stores=new Store[6];
+        Store store1=new Store(37.02028754,30.59819901,30,"bake",5.00);
+        stores[0]=store1;
+        Store store2=new Store(36.88682729,30.7025303,30,"burger",15.00);
+        stores[1]=store2;
+        Store store3=new Store(36.61379416,30.5610624,30,"drink",45.00);
+        stores[2]=store3;
+        Store store4=new Store(36.99647015,30.85289727,30,"cofee",10.00);
+        stores[3]=store4;
+        Store store5=new Store(36.88672206,30.81871253,30,"burger",17.00);
+        stores[4]=store5;
+        Store store6=new Store(36.85133768,30.85056657,30,"cofee",10.00);
+        stores[5]=store6;
+        double[]location=new double[2];
+        location[0]=36.9159143629179;
+        location[1]=30.804464229398153;
+        StoreCalculator storeCalculator=new StoreCalculator(stores,location);
+        System.out.println(storeCalculator.getTravelTimeToOrder());
+
+
     }
     public void communicate() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
